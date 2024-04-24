02a509be0d9997275328d893f30e6736a1055b83
Mahadi Al Hassan <mahadi1141@gmail.com>
2024-04-24 19:37:03 +0600

    Create 1137. N-th Tribonacci Number.cpp

---
+++ Leetcode/DP/1137. N-th Tribonacci Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector<int> dp(n + 1, 1);
+        dp[0] = 0;
+
+        for (int i = 3; i <= n; i++) dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+
+        return dp[n];
+    }
+};
